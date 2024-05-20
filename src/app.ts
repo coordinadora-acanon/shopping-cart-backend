f1b571e69637c97ce4c21a142b917c0e45e102a8
Andres Canon <andres.canon@usegale.cloud>
2024-05-20 09:34:45 -0500

    task: [COO-15] Product Model Implemented

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { CartController } from "./controllers/cart.controller";
+import { ProductController } from "./controllers/product.controller";
 
 const app = express();
 app.use(express.json());
@@ -8,5 +9,7 @@ app.get("/cart", CartController.getCart);
 app.post("/cart", CartController.addItem);
 app.delete("/cart/:id", CartController.removeItem);
 app.delete("/cart", CartController.clearCart);
+
+app.get("/products", ProductController.getProducts);
 
 export default app;
