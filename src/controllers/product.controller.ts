5ab0ed35d9623df5a9a44dec6972433d858ae7e2
Andres Canon <andres.canon@usegale.cloud>
2024-05-20 11:31:57 -0500

    task: [COO-18] Winston Logger implemented

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import logger from "../utils/logger";
 import { ProductService } from "../services/product.service";
 import { ProductRepository } from "../repositories/product.repository";
 
@@ -7,6 +8,13 @@ const productService = new ProductService(productRepository);
 
 export class ProductController {
   static getProducts(req: Request, res: Response): void {
-    res.json(productService.getProducts());
+    try {
+      const products = productService.getProducts();
+      logger.info("Fetching all products", { products });
+      res.json(products);
+    } catch (error) {
+      logger.error("Error fetching all products", { error });
+      res.status(500).json({ error: "Internal server error" });
+    }
   }
 }
