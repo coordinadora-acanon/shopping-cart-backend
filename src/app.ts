80c6049f8f88eeec801dd5c078edbff64cec29c4
Andres Canon <andres.canon@usegale.cloud>
2024-05-20 12:31:11 -0500

    task: [COO-21] CORS Implemented

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express from "express";
+import cors from "cors";
+
 import { CartController } from "./controllers/cart.controller";
 import { ProductController } from "./controllers/product.controller";
 import { requestLogger } from "./middleware/loggerMiddleware";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(requestLogger);
 
 app.get("/cart", CartController.getCart);
