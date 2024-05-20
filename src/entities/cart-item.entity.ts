7998eb50d209f0e367bf0efb30a5177e573a2d62
Andres Canon <andres.canon@usegale.cloud>
2024-05-20 15:19:40 -0500

    task: [COO-23] Cart model refactored

--- src/entities/cart-item.entity.ts
+++ src/entities/cart-item.entity.ts
@@ -1,6 +1,7 @@
 export class CartItem {
   constructor(
     public readonly id: string,
+    public readonly productId: string,
     public readonly name: string,
     public readonly quantity: number,
     public readonly price: number
